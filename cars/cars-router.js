fee43021706f0457f7c682bc817d19f7a8a02c68
Maria Trujillo <mptruj015@gmail.com>
2020-05-12 23:38:41 +0200

    added server, cars router, and index.js

---
+++ cars/cars-router.js
@@ -0,0 +1,52 @@
+const express = require("express");
+
+const db = require("../data/dbConnection.js");
+
+const router = express.Router();
+
+router.get("/", (req,res) => {
+    db("cars")
+    .then(cars => {
+        res.json(cars)
+    })
+    .catch(err => {
+        res.status(500)
+        .json({ message: "Could not get cars info"})
+    })
+})
+
+router.get("/:id", (req,res) => {
+    const { id } = req.params;
+
+    db("cars")
+    .where({ id })
+    .then(car => {
+        res.json(car);
+    })
+    .catch(err => {
+        res.status(500)
+        .json({message: "Failed to access car"})
+    })
+})
+
+router.post("/", (req, res) => {
+    const carData = req.body;
+
+    db("cars")
+    .insert(carData)
+    .then(ids => {
+        db("cars")
+        .where({ id: ids[0]})
+        .then(newCarEntry => {
+            res.status(201)
+            .json(newCarEntry);
+        })
+    })
+    .catch(err => {
+        console.log("POST ERROR", err)
+        res.status(500)
+        .json({ message: "Failed to store data" })
+    })
+})
+
+module.exports = router;
